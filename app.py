8b6378b36c5283015e03b0a91597af7cba420714
dilaseviola-png <dilaseviola@gmail.com>
2026-01-03 15:14:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(page_title="Tes Streamlit")
+st.set_page_config(page_title="Kinetika Orde Reaksi")
 
-st.title("Halo 👋")
-st.write("Kalau ini muncul, berarti deploy berhasil.")
+st.title("Penentuan Orde Reaksi")
+st.write("Upload data waktu dan absorbansi (CSV)")
+
+uploaded_file = st.file_uploader(
+    "Upload file CSV (kolom 1 = waktu, kolom 2 = absorbansi)",
+    type=["csv"]
+)
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Data Input")
+    st.dataframe(df)
+else:
+    st.info("Silakan upload file CSV untuk memulai.")
+
 
